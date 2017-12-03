0e00ea8132b9d7bb1cb07a913eb69e206b92f203
Stephane vialette <vialette@gmail.com>
2017-12-03 21:33:51 +0100

    cleaning Data.FImage.Geometry.Vector.hs

--- src/Data/FImage/Geometry/Vector.hs
+++ src/Data/FImage/Geometry/Vector.hs
@@ -23,32 +23,24 @@ where
 
   -- | Make a vector (dx, dy) from two floats `dx`and `dy`.
   mk :: Float -> Float -> Vector
-  mk vx vy = Vector { dx = vx, dy = vy }
 
   -- | Make a vector '(dz, dz)'' from a float 'dz'.
   mk2 :: Float -> Vector
-  mk2 z = mk z z
 
   -- | Make the vector (-dx, dy) from vector (dx, dy).
   revX :: Vector -> Vector
-  revX v = Vector { dx = - (dx v), dy = dy v }
 
   -- | Make the vector (dx, -dy) from vector (dx, dy).
   revY :: Vector -> Vector
-  revY v = Vector { dx = dx v, dy = - (dy v) }
 
   -- | Make the vector (-dx, -dy) from vector (dx, dy).
   revXY :: Vector -> Vector
-  revXY v = Vector { dx = - (dx v), dy = - (dy v) }
 
   -- | Make the vector (1/dx, dy) from vector (dx, dy).
   invX :: Vector -> Vector
-  invX v = Vector { dx = 1 / dx v, dy = dy v }
 
   -- | Make the vector (dx, 1/dy) from vector (dx, dy).
   invY :: Vector -> Vector
-  invY v = Vector { dx = dx v, dy = 1 / dy v }
 
   -- | Make the vector (1/dx, 1/dy) from vector (dx, dy).
   invXY :: Vector -> Vector
-  invXY v = Vector { dx = 1 / dx v, dy = 1 / dy v }
