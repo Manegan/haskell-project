08e1406172c4c2c6fb466824f377800fce6de2da
Stephane vialette <vialette@gmail.com>
2017-12-03 21:34:35 +0100

    cleaning Data.FImage.Algebra.hs

--- src/Data/FImage/Algebra.hs
+++ src/Data/FImage/Algebra.hs
@@ -16,34 +16,27 @@ where
   -- | The all-True boolean image.
   -- 'universe p = True'
   universe :: BImage.BImage
-  universe = const True
 
   -- | The all-False boolean image.
   -- 'empty p = False'.
   empty :: BImage.BImage
-  empty = const False
 
   -- | Complement a boolean image.
   -- `comp f p = True` iff 'f p = False'.
   comp :: BImage.BImage -> BImage.BImage
-  comp = Lift.lift1 not
 
   -- | Intersection of two boolean images.
   -- 'inter f g p = True' iff 'f p = True' and 'g p = True'.
   inter :: BImage.BImage -> BImage.BImage -> BImage.BImage
-  inter = Lift.lift2 (&&)
 
   -- | Union of two boolean images.
   -- 'union f g p = True' iff 'f p = True' or 'g p = True'.
   union :: BImage.BImage -> BImage.BImage -> BImage.BImage
-  union = Lift.lift2 (||)
 
   -- | Xor of two boolean images.
   -- 'xor f g p = True' iff 'f p /= g p'.
   xor :: BImage.BImage -> BImage.BImage -> BImage.BImage
-  xor = Lift.lift2 (/=)
 
   -- | diff.
   -- 'diff f g p = True' iff 'f p = True' and 'g p = False'.
   diff :: BImage.BImage -> BImage.BImage -> BImage.BImage
-  f `diff` g = f `inter` comp g
